f5cd2415fbc1ec0c520dc088d8caf9a770686f20
Amirhossain Badiei <amir_badiei@yahoo.com>
2025-11-27 21:10:40 +0330

    Login page added

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+import Container from '@/components/shared/layout/Container'
+import React from 'react'
+
+export default function Login() {
+    return (
+        <Container >
+            <div>
+                
+            </div>
+        </Container>
+    )
+}
